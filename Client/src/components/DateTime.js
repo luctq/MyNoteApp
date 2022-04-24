68d3be82f9552b586c9c813d3b2e1f034c331294
Kai <vantienkhai.uet@gmail.com>
2022-04-24 10:43:29 +0700

    add DateTime Component

--- Client/src/components/DateTime.js
+++ Client/src/components/DateTime.js
@@ -1,13 +1,12 @@
 import { StyleSheet, Text, View } from "react-native";
 import React from "react";
+import moment from "moment";
+import "moment/locale/vi";
+
+moment.locale("vi");
 
 export default function DateTime() {
-  function formatTime(time) {
-      return new Date(time);
-  }
-  return (
-      <Text style={styles.time}>{Date.now()}</Text>
-  );
+  return <Text style={styles.time}>{moment("20220423", "YYYYMMDD").fromNow()}</Text>;
 }
 
 const styles = StyleSheet.create({
