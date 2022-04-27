efdf31fc476cbdb6bba3002c2bdce00745d31d27
Kai <vantienkhai.uet@gmail.com>
2022-04-27 16:22:05 +0700

    init folder, note reducer

---
+++ Client/src/redux/reducers/Note.js
@@ -0,0 +1,35 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  noteList: [],
+};
+
+const note = createSlice({
+  name: "note",
+  initialState,
+  reducers: {
+    // các actions
+    addNote(state, action) {
+      const newNote = {
+        id: action.payload.id,
+        folderId: action.payload.folderId,
+        title: action.payload.title,
+        noteContent: action.payload.noteContent,
+        lastEdit: action.payload.lastEdit,
+        isDeleted: action.payload.isDeleted, // hoặc để mặc định là false
+        deleteTime: action.payload.deleteTime, // hoặc để mặc định là null
+      };
+      state.noteList.push(newNote);
+    },
+    deleteNote(state, action) {},
+    editNote(state, action) {},
+  },
+});
+
+const { addNote, deleteNote, editNote } = folder.actions;
+
+export const createNewNote = (info) => (dispatch) => {
+  dispatch(addNote(info));
+};
+
+export default note.reducer;
