132ef1efff359726b8f19125b14db87c60a0adb2
Kai <vantienkhai.uet@gmail.com>
2022-04-27 15:57:32 +0700

    example login with redux

--- Client/App.js
+++ Client/App.js
@@ -1,11 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import NoteListItem from './src/components/NoteListItem'
+import AddNewNoteButton from './src/components/AddNewNoteButton'
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
+      <NoteListItem />
+      <AddNewNoteButton />
     </View>
   );
 }
